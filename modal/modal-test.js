23deda82fd254217822e44b8b7ea09f1ad87f9d6
fernbern <70728008+fernbern@users.noreply.github.com>
2021-02-02 18:28:10 -0800

    Add files via upload

---
+++ modal/modal-test.js
@@ -0,0 +1,24 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
+const modalBtn = docuemnt.querySelector('.modal-btn');
+const modal = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+modalBtn.classList.addEventListener('click', function() {
+  modal.classList.add('open-moda;');
+})
+closeBtn.addEventListener('click', function() {
+  modal.classList.remove('open-modal');
+})
