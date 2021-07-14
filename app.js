6b04529f5a97aecd01addab4d07a35ac430447b8
fernbern <70728008+fernbern@users.noreply.github.com>
2021-07-14 05:58:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 const coloresus = ['yellow','greem'];
-
+const app = 'string';
 btn.addEventListener("click", function () {
   const randomNumber = getRandomNumber();
   // console.log(randomNumber);
